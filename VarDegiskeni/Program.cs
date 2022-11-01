3d79540417fb770f3ad71b2760df8718e0d1b8b1
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-01 10:10:31 +0300

    Add project files.

---
+++ VarDegiskeni/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+var onRenk = ConsoleColor.Green;
+ConsoleColor arkaRenk = ConsoleColor.Red;
+Console.ForegroundColor = onRenk;
+Console.BackgroundColor = arkaRenk;
+
+var x = 100;
+x = x + 200;
+var y = 100.25M;
+float z = 100.25f;
+var deger = true;
+deger = !deger;
+var ad = "Duygu";
+Console.WriteLine("int" + x);
+Console.WriteLine("decimal" + y);
+Console.WriteLine("Mantıksal" + deger);
+Console.WriteLine("Katar" + ad);
+
+
+
